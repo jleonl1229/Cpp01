ca318ce941ed3b36de122f43fd7b37322c2a6a30
Julen Leon Larrondo <jleon-la@c4r8s3.42urduliz.com>
2025-01-19 13:47:15 +0100

    ex04 finished

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -13,17 +13,70 @@ int	main(int ac, char **av)
 	std::string s1 = av[2];
 	std::string s2 = av[3];
 
-	std::fstream file(filename.c_str(), std::ios::in | std::ios::out);
+	std::ifstream file(filename.c_str());
 	if (!file.is_open())
 	{
-		std::cerr << "File could not be opened" << std::endl;
+		std::cerr << "First file could not be opened" << std::endl;
 		return (2);
 	}
 	
+	std::string secondfd = filename.append(".replace");
+	std::ofstream newfile(secondfd.c_str());
+	if (!newfile.is_open())
+	{
+		std::cerr << "Second file could not be opened" << std::endl;
+		return (file.close(), 3);
+	}
+
 	std::string line;
 	while (std::getline(file, line))
 	{
 		std::cout << line << std::endl;
+		int		i;
+		std::string	milestone;
+
+		milestone = "";
+		i = 0;
+		while (line[i])
+		{
+			if (line[i] != ' ')
+				milestone += line[i];		
+			else if (line[i] == ' ')
+			{
+				if (milestone[milestone.length() - 1] == '\n')
+				{
+					std::string	polishedmilestone;
+
+					polishedmilestone = milestone.substr(0, milestone.length() - 1);
+					if (polishedmilestone == s1)
+						newfile << s2;
+				}
+				if (milestone == s1)
+					newfile << s2;
+				else
+					newfile << milestone;
+				milestone = "";
+				newfile << ' ';
+			}
+			i++;
+		}
+		if (milestone != "")
+		{
+			if (milestone[milestone.length() - 1] == '\n')
+			{
+				std::string	polishedmilestone;
+			       
+				polishedmilestone = milestone.substr(0, milestone.length() - 1);
+				if (polishedmilestone == s1)
+					newfile << s2;
+			}
+			if (milestone == s1)
+				newfile << s2;
+			else
+				newfile << milestone;
+			milestone = "";
+		}	
+		newfile << '\n';
 	}
-	return (file.close(), 0);
+	return (file.close(), newfile.close(), 0);
 }
